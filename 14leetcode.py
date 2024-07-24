6f2f2e3bd9232f392626241f7630df4d58810480
xamidovscode <xamidovscode@gmail.com>
2024-07-24 20:07:37 +0500

    14 problem resolved

--- 14leetcode.py
+++ 14leetcode.py
@@ -1,10 +1,21 @@
 def longestCommonPrefix(strs):
-    for i in range(0, len(strs)):
-        for j in strs:
-            print(j[i])
-    return True
+    if not strs:
+        return ""
+
+    min_length = len(min(strs, key=len))
+
+    prefix = ""
+
+    for i in range(min_length):
+        char = strs[0][i]
+
+        if all(s[i] == char for s in strs):
+            prefix += char
+        else:
+            break
+    print(prefix)
+    return prefix
 
 
-strs = ['flower', 'flow', 'flight']
+strs = ["dog", "racecar", "car"]
 longestCommonPrefix(strs)
-
--- 2leetcode.py
+++ 2leetcode.py
@@ -1,38 +1,13 @@
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+class Solution(object):
+
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        print(l1)
+        print(l2)
 
 
-def addTwoNumbers(l1, l2):
-    result_head = ListNode(0)
-    current = result_head
-    carry = 0
 
-    while l1 or l2 or carry:
-        num1 = l1.val if l1 else 0
-        num2 = l2.val if l2 else 0
-        total = num1 + num2 + carry
-        carry = total // 10
-        current.next = ListNode(total % 10)
-        current = current.next
-
-        l1 = l1.next if l1 else None
-        l2 = l2.next if l2 else None
-
-    return result_head.next
-
-
-l1 = ListNode(2)
-l1.next = ListNode(4)
-l1.next.next = ListNode(3)
-
-l2 = ListNode(5)
-l2.next = ListNode(6)
-l2.next.next = ListNode(4)
-
-result = addTwoNumbers(l1, l2)
-
-while result:
-    print(result.val, end=" ")
-    result = result.next

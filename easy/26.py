4dc2a620337e47e56e08e2df0a74427f537ee838
xamidovscode <xamidovcoder@gmail.com>
2024-10-10 12:54:41 +0500

    26

--- easy/26.py
+++ easy/26.py
@@ -1,32 +1,20 @@
 from django.contrib.sitemaps.views import index
 
 
-class Solution(object):
-    @classmethod
-    def remove_duplicates(cls, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        unique = []
+class Solution:
+
+    def remove_duplicates(self, nums):
+        if not nums:
+            return 0
         i = 0
-        count = 0
-        print("F", nums)
-        while i <= len(nums) - 1:
-            element = nums[i]
-            if element not in unique:
-                unique.append(element)
-            else:
-                nums.pop(i - count)
-                count += 1
-                print(i, nums)
-            i += 1
-        return i, unique, nums
+        for j in range(1, len(nums)):
+            if nums[j] != nums[i]:
+                i += 1
+                nums[i] = nums[j]
 
-
+        return i + 1, nums
 obj = Solution()
-function = obj.remove_duplicates([0,0,1,1,1,2,2,3,3,4])
-
+function = obj.remove_duplicates([1,1,2])
 
 print(function)
 

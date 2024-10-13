b6775b671e5cfd8d263f09608e530808bfa40711
xamidovscode <xamidovcoder@gmail.com>
2024-10-13 20:52:53 +0500

    nimadir qildim

--- medium/6.py
+++ medium/6.py
@@ -1,6 +1,16 @@
 def convert(s: str, numRows: int) -> str:
-    print(s.split())
+    is_main = 0
+    i = 0
+    result = ''
+    while i <= len(s) - 1:
+        if is_main == 0:
+            result = result + s[0:numRows]
+            print(result)
+            i += numRows
+            is_main = 1
+        i += 1
+        print(i)
     return s
 
 print(convert("PAYPALISHIRING", 3))
-print('PAHNAPLSIIGYIR')
+# print('PAHNAPLSIIGYIR')

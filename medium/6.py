e3996397c87d0a821914c6f32983b996411af4db
xamidovscode <xamidovcoder@gmail.com>
2024-10-13 21:27:57 +0500

    any changes

--- medium/6.py
+++ medium/6.py
@@ -4,15 +4,14 @@ def convert(s: str, numRows: int) -> str:
     i = 0
     result = ''
     while i <= len(s) - 1:
-        if is_main == 0:
-            result = result + "," + s[0:numRows]
-            i += numRows - 1
+        if not is_main:
+            result = result + "," + s[i:numRows]
+            i += numRows
             is_main = 1
         else:
-            result = result + "," + s[i:i + 1]
+            result = result + "," + s[i]
             is_main = 0
-            i += 2
-        i += 1
+            i += 1
     return result
 
 print(convert("PAYPALISHIRING", 3))

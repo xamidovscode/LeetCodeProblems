6deff51df3cf87620486e20224876c91485593ae
xamidovscode <xamidovcoder@gmail.com>
2024-10-13 17:32:13 +0500

    work for 1 sikle

--- medium/5..py
+++ medium/5..py
@@ -1,29 +1,33 @@
+
 class Solution:
     def longest_palindrome(self, s: str) -> str:
+        i1 = 0
         rst = ""
-        def get_longest_palindrome(a=None):
-            if not a:
-                a = s
-            i1 = 0
-            while i1 <= len(s) -1:
-                substring1 = s[0:i1 + 1]
-                print(substring1, 11111111111111)
-                # if len(substring1) == 1 and len(rst) < 1:
-                #     rst = substring1
-                #     continue
-                # i2 = 0
-                # while i2 <= len(substring1) - 1:
-                #     substring2 = substring1[0:i2 + 1]
-                #     print(substring2)
-                #     i2 += 1
-                i1 += 1
+        substrings = []
+        for i in range(0, len(s) - 1):
+            substrings.append(s[i:])
+        print(substrings)
+        while i1 <= len(s) -1:
+            substring1 = s[0:i1 + 1]
+            print(substring1, 11111111111111)
+            if len(substring1) == 1 and len(rst) < 1:
+                rst = substring1
+                continue
+            i2 = 0
+            while i2 <= len(substring1) - 1:
+                last_index = len(substring1) - 1 - i2
+                if substring1[i2] != substring1[last_index]:
+                    break
+                i2 += 1
             else:
-                return get_longest_palindrome()
+                rst = substring1
+            i1 += 1
+
         return rst
 
 
 obj = Solution()
-function = obj.longest_palindrome("babad")
+function = obj.longest_palindrome("babab")
 
 
 print(function)

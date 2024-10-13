3ca7e7a1744fc485cac2d2927f3ab41700387bc3
xamidovscode <xamidovcoder@gmail.com>
2024-10-13 22:08:49 +0500

    finish

--- medium/7.py
+++ medium/7.py
@@ -4,15 +4,14 @@ def reverse(x: int) -> int:
     y = str(x)
     result = ''
     is_plus = 1
-    if x not in range(-8463947412, 7463847412):
-        return 0
-
     for i in y:
         if i == "-":
             is_plus = 0
         else:
             result = i + result
     result = int(result)
+    if result not in range(-2147483648, 2147483647):
+        return 0
     return result if is_plus else -result
 
-print(reverse(123))
+print(reverse(-123))

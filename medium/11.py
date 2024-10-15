cf77fc8ff374282b4d9ab127010ef270fc661900
xamidovscode <xamidovcoder@gmail.com>
2024-10-15 20:09:08 +0500

    fixed

--- medium/11.py
+++ medium/11.py
@@ -1,21 +1,24 @@
 from typing import List
 
+from typing import List
+
 def max_area(height: List[int]) -> int:
     result = 0
-    i1 = 0
-    main_len = len(height)
-    while i1 < main_len - 1:
-        obj1 = height[i1]
-        i2 = i1 + 1
-        while i2 < main_len:
-            length = i2 - i1
-            obj2 = height[i2]
-            calculated = (obj1 if obj1 < obj2 else obj2) * length
-            if calculated > result:
-                result = calculated
-            i2 += 1
-        i1 += 1
+    i1, i2 = 0, len(height) - 1
+
+    while i1 < i2:
+        width = i2 - i1
+        min_height = min(height[i1], height[i2])
+        calculated = width * min_height
+        result = max(result, calculated)
+
+        if height[i1] < height[i2]:
+            i1 += 1
+        else:
+            i2 -= 1
+
     return result
+
 
 print(max_area([2,3,4,5,18,17,6]))
 

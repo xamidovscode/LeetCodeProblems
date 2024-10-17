817c56a2164c21ddf2749cc10426810248f7db6a
xamidovscode <xamidovcoder@gmail.com>
2024-10-17 15:24:59 +0500

    fixed

---
+++ medium/12.py
@@ -0,0 +1,6 @@
+
+def int_to_roman(num: int) -> str:
+
+    return 'salom'
+
+print(int_to_roman(48))
--- test.py
+++ test.py
@@ -1,3 +1,15 @@
-a = "salom"
 
-print(a[2:4])
+a = 0
+i1 = 2
+result = []
+while i1 <= 1000:
+    i2 = 2
+    print(i1)
+    while i2 <= i1:
+        if i2 // i1 != 0:
+            break
+        result.append(i1)
+        i2 += 1
+    i1 += 1
+
+print(result)

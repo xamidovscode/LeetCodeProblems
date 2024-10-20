6abac49e168b65fa60e8095d074eafa0f84f2a99
xamidovscode <xamidovcoder@gmail.com>
2024-10-20 14:15:17 +0500

    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

---
+++ easy/118.py
@@ -0,0 +1,8 @@
+from typing import List
+
+
+def generate(numRows: int) -> List[List[int]]:
+    return []
+
+
+print(generate(2))
--- easy/202.py
+++ easy/202.py
@@ -1,23 +1,5 @@
 def isHappy(n: int):
     if len(str(n)) == 1 and n != 1 and len(str(n**2)) == 1:
         return False
-    verible = str(n**2)
-    sickle = 0
-    objects = []
-    while True:
-        result = 0
-        if verible in objects:
-            return False
-        for i in verible:
-            obj = int(i)
-            result += obj**2
-        if len(str(result**2)) == 1 and result**2 != 1:
-            return False
-        elif result == 1:
-            return True
-        if result == n**2 and sickle != 0:
-            return False
-        verible = str(result)
-        objects.append(verible)
-        sickle += 1
+    return True
 print(isHappy(5))

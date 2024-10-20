a9af683e43e70d3f89d6015b7f6e29114f04d9d3
xamidovscode <xamidovcoder@gmail.com>
2024-10-20 14:21:57 +0500

    news

--- easy/202.py
+++ easy/202.py
@@ -1,5 +1,16 @@
+
 def isHappy(n: int):
     if len(str(n)) == 1 and n != 1 and len(str(n**2)) == 1:
         return False
-    return True
-print(isHappy(5))
+    obj = str(n)
+    while True:
+        result = 0
+        for i in obj:
+            result += int(i)**2
+        if result == 1:
+            return True
+        if result in (5, 4):
+            return False
+        obj = str(result)
+
+print(isHappy(1111111))

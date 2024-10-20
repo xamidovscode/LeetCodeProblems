e2b2af8010762eeffb0f3b9e3ad563cad6c3786e
xamidovscode <xamidovcoder@gmail.com>
2024-10-20 14:26:53 +0500

    worked

--- easy/202.py
+++ easy/202.py
@@ -1,17 +1,16 @@
 
 def isHappy(n: int):
-    if len(str(n)) == 1 and n != 1 and len(str(n**2)) == 1:
-        return False
-    obj = str(n)
-    while True:
-        result = 0
-        for i in obj:
-            result += int(i)**2
-        if result == 1:
+    seen=set()
+    curr=str(n)
+    while curr not in seen:
+        seen.add(curr)
+        summ=0
+        for digit in curr:
+            summ+=int(digit)**2
+
+        if summ ==1:
             return True
-        if result in (5, 4):
-            return False
-        obj = str(result)
-        print(obj)
+        curr=str(summ)
+    return False
 
 print(isHappy(1111111))

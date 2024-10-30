02ba730fe14056ece2606d2aca2ba5d6b5fe774e
xamidovscode <xamidovcoder@gmail.com>
2024-10-30 21:11:11 +0500

    optimal method

--- easy/9.py
+++ easy/9.py
@@ -1,11 +1,22 @@
+
+
 def isPalindrome(x):
-    x = str(x)
-    for i in range(0, len(x)):
-        result = x[i] == x[len(x) - i - 1]
-        if result is False:
-            return False
-    return True
+    #SLICING METHOD
+    # if x < 0: return False
+    # string = str(x)
+    # length = len(string)
+    # if length == 1: return True
+    # condition, quality = length % 2 , length // 2
+    # if condition == 1:
+    #     obj_1, obj_2 = string[:quality], string[quality + 1:]
+    # else:
+    #     obj_1, obj_2 = string[:quality], string[quality:]
+    # return obj_1 == obj_2[::-1]
+
+    #OPTIMAL METHOD
+    number = str(x)
+    return number == number[::-1]
 
 
-a = isPalindrome(121)
+a = isPalindrome(1001)
 print(a)
